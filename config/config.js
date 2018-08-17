b40c03436421de0813bca1ec9bcae0f4a56a9363
Kae <klramirez665@gmail.com>
2018-08-16 20:23:51 -0700

    Included config file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// config.js
+
+module.exports = {
+    mongo: {
+        admin: {
+            user: "admin",
+            password: "thebestadmin2"
+        },
+        url: "@ds018498.mlab.com:18498/pizzaplace"
+    }
+};
